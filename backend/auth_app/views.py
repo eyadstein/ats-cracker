63d2d0b42a798da75a0577afdfb643802b5f94a1
Eyadstein <eyada5109@gmail.com>
2026-05-26 19:28:58 +0300

    🔧 Add csrf_exempt to auth views to fix 400 Bad Request

--- backend/auth_app/views.py
+++ backend/auth_app/views.py
@@ -1,29 +1,27 @@
 from rest_framework import generics
 from rest_framework_simplejwt.views import TokenObtainPairView
-
 from auth_app.models import User
 from auth_app.serializers import RegisterSerializer, CustomTokenObtainPairSerializer
 import logging
 from rest_framework_simplejwt.views import TokenRefreshView
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
 
-
+@method_decorator(csrf_exempt, name="dispatch")
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = RegisterSerializer
 
+@method_decorator(csrf_exempt, name="dispatch")
 class CustomTokenObtainPairView(TokenObtainPairView):
     serializer_class = CustomTokenObtainPairSerializer
 
-
-
 logger = logging.getLogger(__name__)
 
+@method_decorator(csrf_exempt, name="dispatch")
 class CustomTokenRefreshView(TokenRefreshView):
     def post(self, request, *args, **kwargs):
-        # Log the incoming request body (data)
-        print("Incoming refresh token request dataaaaaaaa: ", request.data)
+        print("Incoming refresh token request data: ", request.data)
         print(request.headers)
-        # Call the parent class's post method
         response = super().post(request, *args, **kwargs)
-
         return response
